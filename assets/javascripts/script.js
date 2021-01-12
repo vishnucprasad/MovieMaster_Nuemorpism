278283f43c468dc1622f77259c051d3c7b8077f7
Vishnu-C-Prasad <mr.vishnucp001@outlook.com>
2021-01-12 21:41:14 +0530

    Login Design, verification design for login and signup

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -26,3 +26,35 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+$('#signupForm').submit((e) => {
+    e.preventDefault();
+    $('#submitBtn').attr('hidden', true);
+    $('#loadingBtn').removeAttr('hidden');
+
+    setTimeout(() => {
+        $("#signupSubmit").hide();
+        $("#verificationForm").slideDown(500);
+    }, 2000);
+});
+
+$('#loginForm').submit((e) => {
+    e.preventDefault();
+    $('#submitBtn').attr('hidden', true);
+    $('#loadingBtn').removeAttr('hidden');
+
+    setTimeout(() => {
+        $("#loginSubmit").hide();
+        $("#verificationForm").slideDown(500);
+    }, 2000);
+});
+
+$('#verificationForm').submit((e) => {
+    e.preventDefault();
+    $('#verificationSubmitBtn').attr('hidden', true);
+    $('#verificationLoadingBtn').removeAttr('hidden');
+
+    setTimeout(() => {
+        $('#verificationLoadingBtn').html('<span class="fa fa-check text-success"></span> Approved');
+    }, 2000);
+});
